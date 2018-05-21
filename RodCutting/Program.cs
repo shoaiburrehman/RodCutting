42ad7b0183ad65262111b3b8ddf940b0847d3e87
shoaiburrehman <39486938+shoaiburrehman@users.noreply.github.com>
2018-05-21 18:57:17 +0500

    Updated Program by execution time

--- RodCutting/Program.cs
+++ RodCutting/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace RodCutting
 {
@@ -10,49 +11,51 @@ namespace RodCutting
     {
         static void Main(string[] args)
         {
+            Stopwatch stopwatch = Stopwatch.StartNew();
             int[] profit = new int[] { 1, 5, 8, 9, 10, 17, 17, 20, 24, 30 };
-            //int result = CutRod(profit, profit.Length);
-            //int result = MemoizedCutRod(profit, profit.Length);
+            //int result1 = CutRod(profit, profit.Length);
+            //int result2 = MemoizedCutRod(profit, profit.Length);
             int result = BottomUpCutRod(profit, profit.Length);
-
             Console.WriteLine("Optimal Solution is:{0}", result);
+            stopwatch.Stop();
+            Console.WriteLine("Execution time for main() is: {0}", stopwatch.Elapsed);
         }
 
         static int CutRod(int[] profit, int n)
         {
+            Stopwatch stopwatch = Stopwatch.StartNew();
             if (n == 0) // first element is 0
                 return 0;
-
             int q = int.MinValue;
-
             for (int i = 1; i <= n; i++)
             {
-                Console.WriteLine("Calculating for {0},{1}", i, n);
+                //Console.WriteLine("Calculating for {0},{1}", i, n);
                 q = Math.Max(q, profit[i - 1] + CutRod(profit, n - i));
             }
-
             //Console.WriteLine("N={0}, Q={1}", n, q);
             Console.WriteLine();
-
+            stopwatch.Stop();
+            Console.WriteLine("The code execution time for CutRod function is: {0}", stopwatch.Elapsed);
             return q;
         }
 
         static int MemoizedCutRod(int[] profit, int n)
         {
+            Stopwatch stopwatch = Stopwatch.StartNew();
             int[] r = new int[n];
             for (int i = 0; i < n; i++)
                 r[i] = int.MinValue;
-
+            stopwatch.Stop();
+            Console.WriteLine("The code execution time for MemoizedCutRod function is: {0}", stopwatch.Elapsed);
             return MemoizedCutRodAux(profit, r, n);
         }
 
         static int MemoizedCutRodAux(int[] profit, int[] r, int n)
         {
+            Stopwatch stopwatch = Stopwatch.StartNew();
             if (n > 0 && r[n - 1] >= 0)
                 return r[n - 1];
-
             int q = int.MinValue;
-
             if (n == 0)
             {
                 q = 0;
@@ -63,14 +66,15 @@ namespace RodCutting
                 for (int i = 1; i <= n; i++)
                     q = Math.Max(q, profit[i - 1] + MemoizedCutRodAux(profit, r, n - i));
             }
-
             r[n - 1] = q;
-
+            stopwatch.Stop();
+            Console.WriteLine("The code execution time for MemoizedCutRodAux function is: {0}", stopwatch.Elapsed);
             return q;
         }
 
         static int BottomUpCutRod(int[] profit, int n)
         {
+            Stopwatch stopwatch = Stopwatch.StartNew();
             int[] r = new int[n + 1]; 
             r[0] = 0;
 
@@ -83,7 +87,8 @@ namespace RodCutting
                 }
                 r[j] = q;
             }
-
+            stopwatch.Stop();
+            Console.WriteLine("The code execution time for BottomUpCutRod function is: {0}", stopwatch.Elapsed);
             return r[n];
         }
     }
